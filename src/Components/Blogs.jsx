1692f4f0720398b1e088441c78e8d716d243977f
Inderpreet02 <inder192002@gmail.com>
2022-12-06 13:20:14 +0530

    markdown under progress

--- src/Components/Blogs.jsx
+++ src/Components/Blogs.jsx
@@ -1,88 +1,47 @@
-import React from "react";
+import { collection, getDocs, onSnapshot, orderBy, query } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { db } from "../firebase";
 import "./Blogs.css";
 
 const Blogs = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const colRef = collection(db, "blogs");
+
+    const q = query(colRef, orderBy("timestamp", "desc"))
+    //real time update
+    onSnapshot(q, (snapshot) => {
+      setBlogs(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      )
+    });
+  }, []);
+  // console.log(blogs.length);
+
   return (
     <div className="blogs">
       <div className="blogs__container">
-        <div className="main__h2">
-          Blogs
-        </div>
+        <div className="main__h2">Blogs</div>
         <div className="blogs__grid">
-          <div className="blog">
-            <div className="blog__top">
-              <img src="https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?cs=srgb&dl=pexels-pixabay-262508.jpg&fm=jpg" alt="" className="blog__img" />
-            </div>
-            <div className="blog__bottom">
-              <div className="blog__title">
-                This is a blogd d d  d d d d d d dd d d dd d 
-              </div>
+          {blogs.map((blog) => {
+            console.log(blog);
+            return (
+              <div className="blog">
+                <div className="blog__top">
+                  <img src={blog.data.img} alt="" className="blog__img" />
+                </div>
+                <div className="blog__bottom">
+                  <div className="blog__title">{blog.data.title}</div>
 
-              <div className="blog__desc">
-                Lorem ipsum dolor sit amet coasdadadasdad adad ad a d asd a da sd asd asd asd  asdaddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd lorem Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed est nam autem laborum recusandae voluptatum? Debitis veniam quod dolorem, saepe voluptatibus reiciendis illum accusamus numquam doloremque eaque voluptatum molestias sint. nsectetur adipisicing elit. At quaerat commodi sequi quos. Id unde quas pariatur atque explicabo nemo animi doloribus, repellendus dicta sequi modi veritatis non vero dolores!
+                  <div className="blog__desc">{blog.data.markdown}</div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div className="blog">
-            <div className="blog__top">
-              <img src="https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?cs=srgb&dl=pexels-pixabay-262508.jpg&fm=jpg" alt="" className="blog__img" />
-            </div>
-            <div className="blog__bottom">
-              <div className="blog__title">This is a blog</div>
-
-              <div className="blog__desc">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. At quaerat commodi sequi quos. Id unde quas pariatur atque explicabo nemo animi doloribus, repellendus dicta sequi modi veritatis non vero dolores!
-              </div>
-            </div>
-          </div>
-          <div className="blog">
-            <div className="blog__top">
-              <img src="https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?cs=srgb&dl=pexels-pixabay-262508.jpg&fm=jpg" alt="" className="blog__img" />
-            </div>
-            <div className="blog__bottom">
-              <div className="blog__title">This is a blog</div>
-
-              <div className="blog__desc">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. At quaerat commodi sequi quos. Id unde quas pariatur atque explicabo nemo animi doloribus, repellendus dicta sequi modi veritatis non vero dolores!
-              </div>
-            </div>
-          </div>
-          <div className="blog">
-            <div className="blog__top">
-              <img src="https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?cs=srgb&dl=pexels-pixabay-262508.jpg&fm=jpg" alt="" className="blog__img" />
-            </div>
-            <div className="blog__bottom">
-              <div className="blog__title">This is a blog</div>
-
-              <div className="blog__desc">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. At quaerat commodi sequi quos. Id unde quas pariatur atque explicabo nemo animi doloribus, repellendus dicta sequi modi veritatis non vero dolores!
-              </div>
-            </div>
-          </div>
-          <div className="blog">
-            <div className="blog__top">
-              <img src="https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?cs=srgb&dl=pexels-pixabay-262508.jpg&fm=jpg" alt="" className="blog__img" />
-            </div>
-            <div className="blog__bottom">
-              <div className="blog__title">This is a blog</div>
-
-              <div className="blog__desc">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. At quaerat commodi sequi quos. Id unde quas pariatur atque explicabo nemo animi doloribus, repellendus dicta sequi modi veritatis non vero dolores!
-              </div>
-            </div>
-          </div>
-          <div className="blog">
-            <div className="blog__top">
-              <img src="https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?cs=srgb&dl=pexels-pixabay-262508.jpg&fm=jpg" alt="" className="blog__img" />
-            </div>
-            <div className="blog__bottom">
-              <div className="blog__title">This is a blog</div>
-
-              <div className="blog__desc">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. At quaerat commodi sequi quos. Id unde quas pariatur atque explicabo nemo animi doloribus, repellendus dicta sequi modi veritatis non vero dolores!
-              </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </div>
     </div>
--- src/Components/MarkdownEditor.jsx
+++ src/Components/MarkdownEditor.jsx
@@ -2,9 +2,32 @@ import React, { useState } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import "./MarkdownEditor.css";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { db } from "../firebase"
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 
 const MarkdownEditor = () => {
   const [markdown, setMarkdown] = useState(`# hello`);
+
+  const handleSubmit = async (e) => {
+
+    e.preventDefault();
+
+    try{
+      const docRef = await addDoc(collection(db, "blogs"), {
+        title: "Markdown Title",
+        markdown: markdown,
+        author: "Joe Hitler",
+        img: "https://images.unsplash.com/photo-1667977602009-ddd5e7b7c2c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+        timestamp: serverTimestamp(),
+      })
+
+      console.log(docRef.id);
+    } catch(e) {
+      console.error("Error adding doc: ", e);
+    }
+  }
   return (
     <div className="markdown">
       <div className="markdown__container">
@@ -22,9 +45,35 @@ const MarkdownEditor = () => {
             ></textarea>
           </div>
           <div className="markdown__right">
-            <ReactMarkdown className="markdown__view" children={markdown} remarkPlugins={[remarkGfm]} />
+            <ReactMarkdown
+              className="markdown__view"
+              children={markdown}
+              remarkPlugins={[remarkGfm]}
+              components={{
+                code({ node, inline, className, children, ...props }) {
+                  const match = /language-(\w+)/.exec(className || "");
+                  return !inline && match ? (
+                    <SyntaxHighlighter
+                      children={String(children).replace(/\n$/, "")}
+                      style={vscDarkPlus}
+                      language={match[1]}
+                      PreTag="div"
+                      {...props}
+                    />
+                  ) : (
+                    <code className={className} {...props}>
+                      {children}
+                    </code>
+                  );
+                },
+              }}
+            />
           </div>
         </div>
+
+        <button className="markdown__btn" onClick={handleSubmit}>
+          Submit
+        </button>
       </div>
     </div>
   );
